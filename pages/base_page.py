5fd4eeb6a3030ec6893d805e0ecc9a711bc83241
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2020-07-02 18:12:13 +0300

    proverka_is_element_present

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,24 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
-	def __init__(self, browser, url):
+	def __init__(self, browser, url, timeout=10):
 		self.browser = browser
 		self.url = url
-	
+		self.browser.implicitly_wait(timeout) #В конструктор BasePage добавим команду
+		#для неявного ожидания со значением по умолчанию в 10
+		
 	def open(self):
 		self.browser.get(self.url)
+
+# реализуем метод is_element_present, в котором будем перехватывать исключение. 
+#В него будем передавать два аргумента: как искать (css, id, xpath и тд) 
+#и собственно что искать (строку-селектор)
+	def is_element_present(self, how, what):
+		try:
+			self.browser.find_element(how, what)
+		except (NoSuchElementException):
+			return False
+		return True
 
 #Теперь в наш класс нужно добавить методы. Первым делом добавим конструктор — метод, который вызывается, когда мы создаем объект. Конструктор объявляется ключевым словом __init__. 
 #В него в качестве параметров мы передаем экземпляр драйвера и url адрес. 
--- pages/main_page.py
+++ pages/main_page.py
@@ -7,6 +7,8 @@ class MainPage(BasePage):
 	def go_to_login_page(self):
 		login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
 		login_link.click()
-
+	def should_be_login_link(self):
+		assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+	
 #В аргументы больше не надо передавать экземпляр браузера, мы его передаем и сохраняем на этапе создания Page Object. 
 #Вместо него нужно указать аргумент self , чтобы иметь доступ к атрибутам и методам класса: 
