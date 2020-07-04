0d46c11a830694824f6cfa92624c41b47de00661
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2020-07-04 20:57:00 +0300

    LoginPage

---
+++ pages/locators.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+#Каждый класс будет соответствовать каждому классу PageObject
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BUTTON_PRODUCT_TO_BASKET = (By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main>h1")
+    MESSAGE_AFTER_PRODUCT_ADD = (By.CSS_SELECTOR, ".alert.alert-safe.alert-noicon.alert-success.fade.in:nth-child(1)")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main p")
+    BASKET_PRICE = (By.CSS_SELECTOR, ".alert.alert-safe.alert-noicon.alert-info.fade.in strong")
