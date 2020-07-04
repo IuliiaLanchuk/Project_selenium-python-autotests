306c6df2b822d5391f8f739646a216ccfc59c792
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2020-07-04 20:58:59 +0300

    modified base_and_main_pages_and_test_main_page_after_loginpage

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,18 +1,16 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import math
 
 class BasePage():
 	def __init__(self, browser, url, timeout=10):
 		self.browser = browser
 		self.url = url
-		self.browser.implicitly_wait(timeout) #В конструктор BasePage добавим команду
-		#для неявного ожидания со значением по умолчанию в 10
+		self.browser.implicitly_wait(timeout)
 		
 	def open(self):
 		self.browser.get(self.url)
 
-# реализуем метод is_element_present, в котором будем перехватывать исключение. 
-#В него будем передавать два аргумента: как искать (css, id, xpath и тд) 
-#и собственно что искать (строку-селектор)
 	def is_element_present(self, how, what):
 		try:
 			self.browser.find_element(how, what)
@@ -20,6 +18,16 @@ class BasePage():
 			return False
 		return True
 
-#Теперь в наш класс нужно добавить методы. Первым делом добавим конструктор — метод, который вызывается, когда мы создаем объект. Конструктор объявляется ключевым словом __init__. 
-#В него в качестве параметров мы передаем экземпляр драйвера и url адрес. 
-#Внутри конструктора сохраняем эти данные как аттрибуты нашего класса.
+	def solve_quiz_and_get_code(self):
+		alert = self.browser.switch_to.alert
+		x = alert.text.split(" ")[2]
+		answer = str(math.log(abs((12 * math.sin(float(x))))))
+		alert.send_keys(answer)
+		alert.accept()
+		try:
+			alert = self.browser.switch_to.alert
+			alert_text = alert.text
+			print(f"Your code: {alert_text}")
+			alert.accept()
+		except NoAlertPresentException:
+			print("No second alert presented")
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,14 +1,19 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
-#импорт базового класса BasePage
+from .locators import MainPageLocators
+from .login_page import LoginPage
 
-#класс  MainPage - наследник класса BasePage(Класс-предок в Python указывается в скобках).класс MainPage будет иметь доступ ко всем атрибутам и методам своего класса-предка
+
+#класс  MainPage - наследник класса BasePage(Класс-предок в Python указывается в скобках).
+#класс MainPage будет иметь доступ ко всем атрибутам и методам своего класса-предка
+
 class MainPage(BasePage): 
-	def go_to_login_page(self):
-		login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-		login_link.click()
-	def should_be_login_link(self):
-		assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
-	
+    def go_to_login_page(self):
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
 #В аргументы больше не надо передавать экземпляр браузера, мы его передаем и сохраняем на этапе создания Page Object. 
 #Вместо него нужно указать аргумент self , чтобы иметь доступ к атрибутам и методам класса: 
